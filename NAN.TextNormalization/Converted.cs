ce70295fba3c78c20d212dd23d4bbce3c7bfc2e9
alexchernyh29 <MSI PULSE@DESKTOP-N44RR9D>
2022-04-02 12:10:02 +0300

    Добавлен класс Convected

---
+++ NAN.TextNormalization/Converted.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NAN.TextNormalization
+{
+    public class Converted
+    {
+        public string Source(string fileMane)
+        {
+            string str;
+            StreamReader reader = new StreamReader(fileMane, Encoding.UTF8);
+            str = reader.ReadToEnd();
+            return str;
+        }
+        public string Convert(string fileMane)
+        {
+            string str;
+            StreamReader reader = new StreamReader(fileMane, Encoding.UTF8);
+            str = Regex.Replace(reader.ReadToEnd(), @"\W", " ").ToLower();
+            str = string.Join(" ", str.Split(new char[] { ' ' }));
+            return str;
+        }
+    }
+}
